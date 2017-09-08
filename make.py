a28dae50ffdf90480fd18d3602f7af361bebc374
Kyubyong Park <longinglove@nate.com>
2017-09-08 12:45:48 +0200

    Add files via upload

--- make.py
+++ make.py
@@ -3,23 +3,21 @@
 
 '''
 Glossary"
- paired sentences: e.g., "I love you", "Ich liebe Dich"
- wx: A string. word of source language
- wy: A string. word of target language
- x: An int. encoded word of source language
- y: An int. encoded word of target language
- collocation: Occurrence of a word in the same sentence.
-    e.g., "love" and "you" or "Ich" and "Dich"
- collocate of (w)x: A word that occurs in the same sentence as x.
-    e.g., collocates of "love" are "I" and "you".
- cooccurrence: (occurrence of) a word in the paired sentence.
-    e.g., cooccurrences of "love" are "Ich", "liebe", and "Dich".
- collocation score of x' on x: #(x and x') / #x
- cooccurrence score of y on x: #(x and y) / #x
+ translation sentence: e.g., "I love you", "Ich liebe Dich"
+ wx: A string. Word of source language X
+ wy: A string. Word of target language Y
+ x: An int. Encoded word of language X
+ y: An int. Encoded word of language Y
+ collocation: A phenomena that two words occur together.
+ collocate of x: A word that occurs with x. Can exist in the same
+    sentence as x, or in the translation sentence of x.
+    the same language as x or not. Here represented as x2.
+    E.g., the collocates of `love` in the above example are
+        `I`, `you`, `Ich`, `liebe`, and `Dich`.
+ collocation prob. of y on x: p(y|x) = cnt(x and y)/cnt(x)
  translation of (w)x: An equivalent of (w)x.
     e.g., Translation of "love" is "liebe".
- translation score of y on x:
-    cooccurrence score - sum of (collocation score*cooccurrence score for every collocate)
+ translation score of y on x: p(y|x) - sum(p(x2s|x)*p(y2s|x2s))
 kyubyong. http://www.github.com/Kyubyong/py-trans
 '''
 from __future__ import print_function
@@ -46,17 +44,11 @@ import argparse
 # lcode2 = args.l2.lower()
 import re
 fs = set([re.search("[a-z]+\-[a-z]+", f).group() for f in glob.glob("data/*")])
-fs = fs - {"en-de"}
-fs = fs - {"bn-en"}
-fs = fs - {"en-no"}
-fs = fs - {"en-it"}
 for f in fs:
     lcode1, lcode2 = f.split("-")
     # lcode1, lcode2 = "en", "hu"
     lcode1, lcode2 = sorted([lcode1, lcode2])
     print("==========", lcode1, lcode2)
-
-
 
     # Special tokenizers
     if "ko" in (lcode1, lcode2):
@@ -116,49 +108,43 @@ for f in fs:
 
         return words
 
-    print("# get normalized sents")
-    def _get_sents(lcode):
-        sents = []
-        for line in tqdm(codecs.open('data/OpenSubtitles2016.{}-{}.{}'.format(lcode1, lcode2, lcode), 'r', 'utf-8').read().split("\n")):
-            sent = normalize(line, lcode)
-            sent = word_segment(sent, lcode)
-            sents.append(sent)
-        return sents
-
-    print("## making sentences of lcode1 ...")
-    sents1 = _get_sents(lcode1)
-    print("## making sentences of lcode2 ...")
-    sents2 = _get_sents(lcode2)
+    print("# normalize")
+    print("\t## lcode1 ...")
+    sents1 = [word_segment(normalize(line, lcode1), lcode1)
+                for line in tqdm(
+                        codecs.open('data/OpenSubtitles2016.{}-{}.{}'.format(lcode1, lcode2, lcode1),
+                        'r', 'utf-8').read().split("\n"))]
+    print("\t## lcode2 ...")
+    sents2 = [word_segment(normalize(line, lcode2), lcode2)
+              for line in tqdm(
+            codecs.open('data/OpenSubtitles2016.{}-{}.{}'.format(lcode1, lcode2, lcode2),
+                        'r', 'utf-8').read().split("\n"))]
 
     assert len(sents1)==len(sents2), \
         """{} and {} MUST be the same in length.\n
            {} has {} lines, but {} has {} lines""".format(lcode1, lcode2,
                                                           lcode1, len(sents1),
                                                           lcode2, len(sents2))
-    print("# Make folder")
+    # Create folder
     if not os.path.exists('outputs/{}-{}'.format(lcode1, lcode2)): os.makedirs('outputs/{}-{}'.format(lcode1, lcode2))
 
     print("# Initialize dictionaries")
     # conversion dictionaries
+    print("\t## lcode1...")
     wx_to_x, x_to_wx, x_to_cnt = dict(), dict(), dict()
-    wx_to_cnt = Counter(list(chain.from_iterable(sents1)))
+    wx_to_cnt = Counter(tqdm(list(chain.from_iterable(sents1))))
     for x, (wx, cnt) in enumerate(wx_to_cnt.most_common(len(wx_to_cnt))): # sorting is useful.
         wx_to_x[wx] = x
         x_to_wx[x] = wx
         x_to_cnt[x] = cnt
 
+    print("\t## lcode2...")
     wy_to_y, y_to_wy, y_to_cnt = dict(), dict(), dict()
-    wy_to_cnt = Counter(list(chain.from_iterable(sents2)))
+    wy_to_cnt = Counter(tqdm(list(chain.from_iterable(sents2))))
     for y, (wy, cnt) in enumerate(wy_to_cnt.items()):
         wy_to_y[wy] = y
         y_to_wy[y] = wy
         y_to_cnt[y] = cnt
-
-    # For sanity check
-    if lcode1=="en":
-        time_id = wx_to_x.get('time', None)
-    elif lcode2=="en":
-        time_id = wy_to_y.get('time', None)
 
     # collocation dictionaries
     x_to_x2s = dict() # x2s: collocates of x. {x: {x2: cnt, x2: cnt, ...}}
@@ -178,23 +164,19 @@ for f in fs:
         ys = [wy_to_y[wy] for wy in sent2]
 
         # Collocation dictionary updates
-        ## lcode1 -> lcode2
-        for x in xs:
-            for x2 in xs: # x2: collocate
-                if x==x2: continue
-                if x2 > 1000: continue # Prune infrequent words
-                if x not in x_to_x2s: x_to_x2s[x] = dict()
-                if x2 not in x_to_x2s[x]: x_to_x2s[x][x2] = 0
-                x_to_x2s[x][x2] += 1
+        def _update(xs, x_to_x2s):
+            for x in xs:
+                for x2 in xs:  # x2: collocate
+                    if x == x2: continue
+                    if x2 > 1000: continue  # Cut off infrequent words to save memory
+                    if x not in x_to_x2s: x_to_x2s[x] = dict()
+                    if x2 not in x_to_x2s[x]: x_to_x2s[x][x2] = 0
+                    x_to_x2s[x][x2] += 1
 
-        # lcode1 <- lcode2
-        for y in ys:
-            for y2 in ys:
-                if y==y2: continue
-                if y2 > 1000: continue
-                if y not in y_to_y2s: y_to_y2s[y] = dict()
-                if y2 not in y_to_y2s[y]: y_to_y2s[y][y2] = 0
-                y_to_y2s[y][y2] += 1
+        ## lcode1
+        _update(xs, x_to_x2s)
+        ## lcode2
+        _update(ys, y_to_y2s)
 
         # Translation dictionary updates
         for x in xs:
@@ -209,7 +191,7 @@ for f in fs:
                     if y not in y_to_xs: y_to_xs[y] = dict()
                     if x not in y_to_xs[y]: y_to_xs[y][x] = 0
                     y_to_xs[y][x] += 1
-                else:
+                else: # We don't add new words after some point to save memory.
                     ## lcode1 -> lcode2
                     if x in x_to_ys and \
                        y in x_to_ys[x] and \
@@ -222,63 +204,49 @@ for f in fs:
 
         line_num += 1
 
-    print("# Adjust and fix final translations...")
+    print("# Adjust ...")
     def _get_trans(x_to_ys, x_to_cnt, x_to_x2s):
         x_to_trans = dict()
         for x, ys in tqdm(x_to_ys.items()):
+            if x not in x_to_x2s: continue # if there's no collocate, we don't have to adjust the score.
             cntx = x_to_cnt[x]
             y_scores = []
-            if x==time_id: print(cntx)
-            # if x == time_id: print("freq. of time =", cntx) # for sanity check
-
             for y, cnty in sorted(ys.items(), key=operator.itemgetter(1), reverse=True)[:20]:  # 20: heuristic
-                if x==time_id:print("y=====",y)
-                translation_score = cnty / float(cntx)  # initial value
-                # if x == time_id and x2 == the_id:
-                #     print("\n\ntrans=", y_to_wy[y]);
-                #     print("translation_score=", translation_score)
-                if x in x_to_x2s:
-                    for x2, cntx2 in x_to_x2s[x].items():  # Collocates
-                        # if x == time_id and x2 == the_id:
-                        #     print("collocate=", x_to_wx[x2])
-                        p_x_x2 = cntx2 / float(cntx)
-                        p_x2_y2 = 0
-                        if x2 in x_to_ys:
-                            p_x2_y2 = x_to_ys[x2].get(y, 0) / float(x_to_cnt[x2])
-                        translation_score -= (p_x_x2 * p_x2_y2)
-                        # if x == time_id and x2 == the_id:
-                        #     print("collocation score=", collocation_score, cntx2)
-                        #     print("coocurrence_score=", coocurrence_score, x_to_ys[x2].get(y, 0, ), float(x_to_cnt[x2]))
-                        #     print("minus score=", collocation_score * coocurrence_score)
-                # if x == time_id:
-                #     print("\n=====\n", "finale translation score=", translation_score)
-                y_scores.append((y, translation_score))
-                if x==time_id:print("yscore:", y_scores)
-            # if x==time_id:print("y_scores:", y_scores)
+                ts = cnty / float(cntx)  # translation score: initial value
+                for x2, cntx2 in x_to_x2s[x].items():  # Collocates
+                    p_x_x2 = cntx2 / float(cntx)
+                    p_x2_y2 = 0
+                    if x2 in x_to_ys:
+                        p_x2_y2 = x_to_ys[x2].get(y, 0) / float(x_to_cnt[x2])
+                    ts -= (p_x_x2 * p_x2_y2)
+                y_scores.append((y, ts))
             trans = sorted(y_scores, key=lambda x: x[1], reverse=True)[:10]  # 10: heuristic
             trans = [each[0] for each in trans]
-            if x==time_id:print("x:", x, "trans:", trans)
-
             x_to_trans[x] = trans
-            if x==time_id:print(x_to_trans[x])
+
         return x_to_trans
 
-    print("## lcode1 -> lcode2")
+    print("\t## lcode1 -> lcode2")
     x_to_trans = _get_trans(x_to_ys, x_to_cnt, x_to_x2s)
-    print("## lcode1 <- lcode2")
+    print("\t## lcode1 <- lcode2")
     y_to_trans = _get_trans(y_to_xs, y_to_cnt, y_to_y2s)
 
-
     print("# sanity check")
-    if time_id is not None:
+    def _sanity_check(wx_to_x, x_to_ys, x_to_trans, y_to_wy):
+        if "time" not in wx_to_x: return ""
+        time_id = wx_to_x["time"]
+
         # before adjustment
         ys = x_to_ys[time_id]
         y_cnt = sorted(ys.items(), key=operator.itemgetter(1), reverse=True)[:20]
-        print("before adjustment the translations of `time` were =>", " | ".join(y_to_wy[y] for y, cnt in y_cnt))
+        print("\tbefore adjustment the translations of `time` were =>", " | ".join(y_to_wy[y] for y, cnt in y_cnt))
 
         # after adjustment
         ys = x_to_trans[time_id]
-        print("after adjustment the translations of `time` are => ", " | ".join(y_to_wy[y] for y in ys))
+        print("\tafter adjustment the translations of `time` are => ", " | ".join(y_to_wy[y] for y in ys))
+
+    if lcode1=="en": _sanity_check(wx_to_x, x_to_ys, x_to_trans, y_to_wy)
+    elif lcode2=="en": _sanity_check(wy_to_y, y_to_xs, y_to_trans, x_to_wx)
 
     print("# Write")
     ## lexicons

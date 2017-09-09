63baa2a3e7a9ca131ed2dface6b21197b9aaf3b8
Kyubyong Park <longinglove@nate.com>
2017-09-09 14:41:50 +0200

    Add files via upload

--- make.py
+++ make.py
@@ -38,223 +38,245 @@ import argparse
 #                                      'See `http://opus.lingfil.uu.se/OpenSubtitles2016.php`""")
 # parser.add_argument('--l2', help="""ISO 639-1 code of target language. \n
 #                                      'See `http://opus.lingfil.uu.se/OpenSubtitles2016.php`""")
+# parser.add_argument('--n', help="number of lines that are used", default=30000000)
+# parser.add_argument('--m', help="rate of word counts that count as lexicon", default=.8)
+# parser.add_argument('--o', help="number of words that are used in calculating collocation", default=1000)
+# parser.add_argument('--p', help="number of line that used as initial contstructor", default=100000)
 # args = parser.parse_args()
 #
 # lcode1 = args.l1.lower()
 # lcode2 = args.l2.lower()
-import re
-fs = set([re.search("[a-z]+\-[a-z]+", f).group() for f in glob.glob("data/*")])
-for f in fs:
-    lcode1, lcode2 = f.split("-")
-    # lcode1, lcode2 = "en", "hu"
-    lcode1, lcode2 = sorted([lcode1, lcode2])
-    print("==========", lcode1, lcode2)
+# n = args.n
+# m = args.m
+# o = args.o
+# p = args.p
 
-    # Special tokenizers
-    if "ko" in (lcode1, lcode2):
-        from konlpy.tag import Kkma  # pip install konlpy. See http://konlpy.org/en/v0.4.4/ for further information.
-        kkma = Kkma()
-    elif "ja" in (lcode1, lcode2):
-        import MeCab  # See https://pypi.python.org/pypi/mecab-python/0.996
-        mecab = MeCab.Tagger("-Owakati")
-    elif "zh" in (lcode1, lcode2):
-        import jieba
-    elif "vi" in (lcode1, lcode2):
-        from pyvi.pyvi import ViTokenizer
-    elif "th" in (lcode1, lcode2):
-        import pythai
+# import re
+# fs = set([re.search("[a-z]+\-[a-z]+", f).group() for f in glob.glob("data/*")])
+# fs = fs - {"bn-en"}
+# fs = fs - {"en-no"}
+# fs = fs - {"en-eo"}
+# fs = fs - {"en-nl"}
+# fs = fs - {"en-sv"}
+# fs = fs - {"en-hu"}
+# fs = fs - {"en-it"}
+# fs = fs - {"en-hi"}
+# fs = fs - {"en-ms"}
+# fs = fs - {"en-ru"}
+# fs = fs - {"ca-en"}
+# for f in fs:
+# lcode1, lcode2 = f.split("-")
+lcode1, lcode2 = "en", "nl"
+# lcode1, lcode2 = sorted([lcode1, lcode2])
+print("==========", lcode1, lcode2)
 
-    def normalize(text, lcode):
-        if lcode in ['ko']:  # korean
-            text = regex.sub(u"[^ \p{Hangul}]", " ", text)  # Replace unacceptable characters with a space.
-        elif lcode in ['ja']:  # japanese
-            text = regex.sub(u"[^\p{Han}\p{Hiragana}\p{Katakana}ー]", "", text)
-        elif lcode in ['zh']:  # chinsese
-            text = regex.sub(u"[^\p{Han}]", "", text)
-        elif lcode in ['th']:  # thai
-            text = regex.sub(u"[^ \p{Thai}]", " ", text)
-        elif lcode in ['ru']:  # russian
-            text = regex.sub(u"[^ \p{Cyrillic}\-]", " ", text)
-            text = text.lower()
-        # elif lcode in ['ar']: # arabic
-        #         text = regex.sub(u"[^ \p{Arabic}.?!\-]", " ", text)
-        elif lcode in ['hi']:  # hindi
-            text = regex.sub(u"[^ \p{Devanagari}\-]", " ", text)
-        elif lcode in ['bn']:  # bengali
-            text = regex.sub(u"[^ \p{Bengali}\-]", " ", text)
-        elif lcode in ['de']:  # german
-            text = regex.sub(u"[^ \p{Latin}\-']", " ", text)
-        else:  # Mostly european languages
-            text = regex.sub(u"[^ \p{Latin}\-']", " ", text)
-            text = text.lower()
+# Special tokenizers
+if "ko" in (lcode1, lcode2):
+    from konlpy.tag import Kkma  # pip install konlpy. See http://konlpy.org/en/v0.4.4/ for further information.
+    kkma = Kkma()
+elif "ja" in (lcode1, lcode2):
+    import MeCab  # See https://pypi.python.org/pypi/mecab-python/0.996
+    mecab = MeCab.Tagger("-Owakati")
+elif "zh" in (lcode1, lcode2):
+    import jieba
+elif "vi" in (lcode1, lcode2):
+    from pyvi.pyvi import ViTokenizer
+elif "th" in (lcode1, lcode2):
+    import pythai
 
-        # Common
-        text = regex.sub("[ ]{2,}", " ", text)  # Squeeze spaces.
-        return text
+def normalize(text, lcode):
+    if lcode in ['ko']:  # korean
+        text = regex.sub(u"[^ \p{Hangul}]", " ", text)  # Replace unacceptable characters with a space.
+    elif lcode in ['ja']:  # japanese
+        text = regex.sub(u"[^\p{Han}\p{Hiragana}\p{Katakana}ー]", "", text)
+    elif lcode in ['zh']:  # chinsese
+        text = regex.sub(u"[^\p{Han}]", "", text)
+    elif lcode in ['th']:  # thai
+        text = regex.sub(u"[^ \p{Thai}]", " ", text)
+    elif lcode in ['ru']:  # russian
+        text = regex.sub(u"[^ \p{Cyrillic}\-]", " ", text)
+        text = text.lower()
+    # elif lcode in ['ar']: # arabic
+    #         text = regex.sub(u"[^ \p{Arabic}.?!\-]", " ", text)
+    elif lcode in ['hi']:  # hindi
+        text = regex.sub(u"[^ \p{Devanagari}\-]", " ", text)
+    elif lcode in ['bn']:  # bengali
+        text = regex.sub(u"[^ \p{Bengali}\-]", " ", text)
+    elif lcode in ['de']:  # german
+        text = regex.sub(u"[^ \p{Latin}\-']", " ", text)
+    else:  # Mostly european languages
+        text = regex.sub(u"[^ \p{Latin}\-']", " ", text)
+        text = text.lower()
 
-    def word_segment(sent, lcode):
-        if lcode in ['ko']:
-            words = [word for word, _ in kkma.pos(sent)]
-        # elif lcode in ['ja']:
-        #     words = mecab.parse(sent.encode('utf8')).split()
-        # elif lcode in ['th']:
-        #     words = pythai.split(sent)
-        elif lcode in ['vi']:
-            words = ViTokenizer.tokenize(sent).split()
-        elif lcode in ['zh']:
-            words = list(jieba.cut(sent, cut_all=False))
-        else:  # Mostly european languages
-            words = sent.split()
+    # Common
+    text = regex.sub("[ ]{2,}", " ", text)  # Squeeze spaces.
+    return text
 
-        return words
+def word_segment(sent, lcode):
+    if lcode in ['ko']:
+        words = [word for word, _ in kkma.pos(sent)]
+    # elif lcode in ['ja']:
+    #     words = mecab.parse(sent.encode('utf8')).split()
+    # elif lcode in ['th']:
+    #     words = pythai.split(sent)
+    elif lcode in ['vi']:
+        words = ViTokenizer.tokenize(sent).split()
+    elif lcode in ['zh']:
+        words = list(jieba.cut(sent, cut_all=False))
+    else:  # Mostly european languages
+        words = sent.split()
 
-    print("# normalize")
-    print("\t## lcode1 ...")
-    sents1 = [word_segment(normalize(line, lcode1), lcode1)
-                for line in tqdm(
-                        codecs.open('data/OpenSubtitles2016.{}-{}.{}'.format(lcode1, lcode2, lcode1),
-                        'r', 'utf-8').read().split("\n"))]
-    print("\t## lcode2 ...")
-    sents2 = [word_segment(normalize(line, lcode2), lcode2)
-              for line in tqdm(
-            codecs.open('data/OpenSubtitles2016.{}-{}.{}'.format(lcode1, lcode2, lcode2),
-                        'r', 'utf-8').read().split("\n"))]
+    return words
 
-    assert len(sents1)==len(sents2), \
-        """{} and {} MUST be the same in length.\n
-           {} has {} lines, but {} has {} lines""".format(lcode1, lcode2,
-                                                          lcode1, len(sents1),
-                                                          lcode2, len(sents2))
-    # Create folder
-    if not os.path.exists('outputs/{}-{}'.format(lcode1, lcode2)): os.makedirs('outputs/{}-{}'.format(lcode1, lcode2))
+print("Step0. Normalize sentences")
+print("\t## lcode1 ...")
+sents1 = [word_segment(normalize(line, lcode1), lcode1)
+            for line in tqdm(
+                    codecs.open('data/OpenSubtitles2016.{}-{}.{}'.format(lcode1, lcode2, lcode1),
+                    'r', 'utf-8').read().split("\n")[:300000000])]
+print("\t## lcode2 ...")
+sents2 = [word_segment(normalize(line, lcode2), lcode2)
+          for line in tqdm(
+        codecs.open('data/OpenSubtitles2016.{}-{}.{}'.format(lcode1, lcode2, lcode2),
+                    'r', 'utf-8').read().split("\n"))]
 
-    print("# Initialize dictionaries")
-    # conversion dictionaries
-    print("\t## lcode1...")
-    wx_to_x, x_to_wx, x_to_cnt = dict(), dict(), dict()
-    wx_to_cnt = Counter(tqdm(list(chain.from_iterable(sents1))))
-    for x, (wx, cnt) in enumerate(wx_to_cnt.most_common(len(wx_to_cnt))): # sorting is useful.
-        wx_to_x[wx] = x
-        x_to_wx[x] = wx
-        x_to_cnt[x] = cnt
+assert len(sents1)==len(sents2), \
+    """{} and {} MUST be the same in length.\n
+       {} has {} lines, but {} has {} lines""".format(lcode1, lcode2,
+                                                      lcode1, len(sents1),
+                                                      lcode2, len(sents2))
+# Create folder
+if not os.path.exists('outputs/{}-{}'.format(lcode1, lcode2)): os.makedirs('outputs/{}-{}'.format(lcode1, lcode2))
 
-    print("\t## lcode2...")
-    wy_to_y, y_to_wy, y_to_cnt = dict(), dict(), dict()
-    wy_to_cnt = Counter(tqdm(list(chain.from_iterable(sents2))))
-    for y, (wy, cnt) in enumerate(wy_to_cnt.items()):
-        wy_to_y[wy] = y
-        y_to_wy[y] = wy
-        y_to_cnt[y] = cnt
+print("Step1. Initialize dictionaries")
+# conversion dictionaries
+print("\t## lcode1...")
+wx_to_x, x_to_wx, x_to_cnt = dict(), dict(), dict()
+wx_to_cnt = Counter(tqdm(list(chain.from_iterable(sents1))))
+for x, (wx, cnt) in enumerate(wx_to_cnt.most_common(len(wx_to_cnt))): # sorting is useful.
+    if x > .8*len(wx_to_cnt): break
+    wx_to_x[wx] = x
+    x_to_wx[x] = wx
+    x_to_cnt[x] = cnt
 
-    # collocation dictionaries
-    x_to_x2s = dict() # x2s: collocates of x. {x: {x2: cnt, x2: cnt, ...}}
-    y_to_y2s = dict() # y2s: collocates of y. {y: {y2: cnt, y2: cnt, ...}}
+print("\t## lcode2...")
+wy_to_y, y_to_wy, y_to_cnt = dict(), dict(), dict()
+wy_to_cnt = Counter(tqdm(list(chain.from_iterable(sents2))))
+for y, (wy, cnt) in enumerate(wy_to_cnt.most_common(len(wy_to_cnt))):
+    if y > .8*len(wy_to_cnt): break
+    wy_to_y[wy] = y
+    y_to_wy[y] = wy
+    y_to_cnt[y] = cnt
 
-    # translation dictionaries
-    x_to_ys = dict() # {x: {y: cnt, y: cnt, ...}}
-    y_to_xs = dict() # {y: {x: cnt, x: cnt, ...}}
+# collocation dictionaries
+x_to_x2s = dict() # x2s: collocates of x. {x: {x2: cnt, x2: cnt, ...}}
+y_to_y2s = dict() # y2s: collocates of y. {y: {y2: cnt, y2: cnt, ...}}
 
-    print("# Update dictionaries ...")
-    line_num = 1
-    for sent1, sent2 in tqdm(zip(sents1, sents2)):
-        if len(sent1) <= 1 or len(sent2) <= 1: continue
+# translation dictionaries
+x_to_ys = dict() # {x: {y: cnt, y: cnt, ...}}
+y_to_xs = dict() # {y: {x: cnt, x: cnt, ...}}
 
-        # To indices
-        xs = [wx_to_x[wx] for wx in sent1]
-        ys = [wy_to_y[wy] for wy in sent2]
+print("Step2. Update dictionaries ...")
+line_num = 1
+for sent1, sent2 in tqdm(zip(sents1, sents2)):
+    if len(sent1) <= 1 or len(sent2) <= 1: continue
 
-        # Collocation dictionary updates
-        def _update(xs, x_to_x2s):
-            for x in xs:
-                for x2 in xs:  # x2: collocate
-                    if x == x2: continue
-                    if x2 > 1000: continue  # Cut off infrequent words to save memory
-                    if x not in x_to_x2s: x_to_x2s[x] = dict()
-                    if x2 not in x_to_x2s[x]: x_to_x2s[x][x2] = 0
-                    x_to_x2s[x][x2] += 1
+    # To indices
+    xs = [wx_to_x[wx] for wx in sent1 if wx in wx_to_x]
+    ys = [wy_to_y[wy] for wy in sent2 if wy in wy_to_y]
 
-        ## lcode1
-        _update(xs, x_to_x2s)
-        ## lcode2
-        _update(ys, y_to_y2s)
-
-        # Translation dictionary updates
+    # Collocation dictionary updates
+    def _update(xs, x_to_x2s):
         for x in xs:
-            for y in ys:
-                if line_num <= 100000:
-                    ## lcode1 -> lcode2
-                    if x not in x_to_ys: x_to_ys[x] = dict()
-                    if y not in x_to_ys[x]: x_to_ys[x][y] = 0
-                    x_to_ys[x][y] += 1
+            for x2 in xs:  # x2: collocate
+                if x == x2: continue
+                if x2 > 1000: continue  # Cut off infrequent words to save memory
+                if x not in x_to_x2s: x_to_x2s[x] = dict()
+                if x2 not in x_to_x2s[x]: x_to_x2s[x][x2] = 0
+                x_to_x2s[x][x2] += 1
 
-                    ## lcode1 <- lcode2
-                    if y not in y_to_xs: y_to_xs[y] = dict()
-                    if x not in y_to_xs[y]: y_to_xs[y][x] = 0
-                    y_to_xs[y][x] += 1
-                else: # We don't add new words after some point to save memory.
-                    ## lcode1 -> lcode2
-                    if x in x_to_ys and \
-                       y in x_to_ys[x] and \
-                       x_to_ys[x][y] > 1: x_to_ys[x][y] += 1
+    ## lcode1
+    _update(xs, x_to_x2s)
+    ## lcode2
+    _update(ys, y_to_y2s)
 
-                    ## lcode1 <- lcode2
-                    if y in y_to_xs and \
-                       x in y_to_xs[y] and \
-                       y_to_xs[y][x] > 1: y_to_xs[y][x] += 1
+    # Translation dictionary updates
+    for x in xs:
+        for y in ys:
+            if line_num <= 100000:
+                ## lcode1 -> lcode2
+                if x not in x_to_ys: x_to_ys[x] = dict()
+                if y not in x_to_ys[x]: x_to_ys[x][y] = 0
+                x_to_ys[x][y] += 1
 
-        line_num += 1
+                ## lcode1 <- lcode2
+                if y not in y_to_xs: y_to_xs[y] = dict()
+                if x not in y_to_xs[y]: y_to_xs[y][x] = 0
+                y_to_xs[y][x] += 1
+            else: # We don't add new words after some point to save memory.
+                ## lcode1 -> lcode2
+                if x in x_to_ys and \
+                   y in x_to_ys[x] and \
+                   x_to_ys[x][y] > 1: x_to_ys[x][y] += 1
 
-    print("# Adjust ...")
-    def _get_trans(x_to_ys, x_to_cnt, x_to_x2s):
-        x_to_trans = dict()
-        for x, ys in tqdm(x_to_ys.items()):
-            if x not in x_to_x2s: continue # if there's no collocate, we don't have to adjust the score.
-            cntx = x_to_cnt[x]
-            y_scores = []
-            for y, cnty in sorted(ys.items(), key=operator.itemgetter(1), reverse=True)[:20]:  # 20: heuristic
-                ts = cnty / float(cntx)  # translation score: initial value
-                for x2, cntx2 in x_to_x2s[x].items():  # Collocates
-                    p_x_x2 = cntx2 / float(cntx)
-                    p_x2_y2 = 0
-                    if x2 in x_to_ys:
-                        p_x2_y2 = x_to_ys[x2].get(y, 0) / float(x_to_cnt[x2])
-                    ts -= (p_x_x2 * p_x2_y2)
-                y_scores.append((y, ts))
-            trans = sorted(y_scores, key=lambda x: x[1], reverse=True)[:10]  # 10: heuristic
-            trans = [each[0] for each in trans]
-            x_to_trans[x] = trans
+                ## lcode1 <- lcode2
+                if y in y_to_xs and \
+                   x in y_to_xs[y] and \
+                   y_to_xs[y][x] > 1: y_to_xs[y][x] += 1
 
-        return x_to_trans
+    line_num += 1
 
-    print("\t## lcode1 -> lcode2")
-    x_to_trans = _get_trans(x_to_ys, x_to_cnt, x_to_x2s)
-    print("\t## lcode1 <- lcode2")
-    y_to_trans = _get_trans(y_to_xs, y_to_cnt, y_to_y2s)
+print("# Adjust ...")
+def _get_trans(x_to_ys, x_to_cnt, x_to_x2s):
+    x_to_trans = dict()
+    for x, ys in tqdm(x_to_ys.items()):
+        if x not in x_to_x2s: continue # if there's no collocate, we don't have to adjust the score.
+        cntx = x_to_cnt[x]
+        y_scores = []
+        for y, cnty in sorted(ys.items(), key=operator.itemgetter(1), reverse=True)[:20]:  # 20: heuristic
+            ts = cnty / float(cntx)  # translation score: initial value
+            for x2, cntx2 in x_to_x2s[x].items():  # Collocates
+                p_x_x2 = cntx2 / float(cntx)
+                p_x2_y2 = 0
+                if x2 in x_to_ys:
+                    p_x2_y2 = x_to_ys[x2].get(y, 0) / float(x_to_cnt[x2])
+                ts -= (p_x_x2 * p_x2_y2)
+            y_scores.append((y, ts))
+        trans = sorted(y_scores, key=lambda x: x[1], reverse=True)[:10]  # 10: heuristic
+        trans = [each[0] for each in trans]
+        x_to_trans[x] = trans
 
-    print("# sanity check")
-    def _sanity_check(wx_to_x, x_to_ys, x_to_trans, y_to_wy):
-        if "time" not in wx_to_x: return ""
-        time_id = wx_to_x["time"]
+    return x_to_trans
 
-        # before adjustment
-        ys = x_to_ys[time_id]
-        y_cnt = sorted(ys.items(), key=operator.itemgetter(1), reverse=True)[:20]
-        print("\tbefore adjustment the translations of `time` were =>", " | ".join(y_to_wy[y] for y, cnt in y_cnt))
+print("\t## lcode1 -> lcode2")
+x_to_trans = _get_trans(x_to_ys, x_to_cnt, x_to_x2s)
+print("\t## lcode1 <- lcode2")
+y_to_trans = _get_trans(y_to_xs, y_to_cnt, y_to_y2s)
 
-        # after adjustment
-        ys = x_to_trans[time_id]
-        print("\tafter adjustment the translations of `time` are => ", " | ".join(y_to_wy[y] for y in ys))
+print("# sanity check")
+def _sanity_check(wx_to_x, x_to_ys, x_to_trans, y_to_wy):
+    if "time" not in wx_to_x: return ""
+    time_id = wx_to_x["time"]
 
-    if lcode1=="en": _sanity_check(wx_to_x, x_to_ys, x_to_trans, y_to_wy)
-    elif lcode2=="en": _sanity_check(wy_to_y, y_to_xs, y_to_trans, x_to_wx)
+    # before adjustment
+    ys = x_to_ys[time_id]
+    y_cnt = sorted(ys.items(), key=operator.itemgetter(1), reverse=True)[:20]
+    print("\tbefore adjustment the translations of `time` were =>", " | ".join(y_to_wy[y] for y, cnt in y_cnt))
 
-    print("# Write")
-    ## lexicons
-    pickle.dump((wx_to_x, x_to_wx), open('outputs/{}-{}/{}.lexicon.pkl'.format(lcode1, lcode2, lcode1), 'wb'))
-    pickle.dump((wy_to_y, y_to_wy), open('outputs/{}-{}/{}.lexicon.pkl'.format(lcode1, lcode2, lcode2), 'wb'))
+    # after adjustment
+    ys = x_to_trans[time_id]
+    print("\tafter adjustment the translations of `time` are => ", " | ".join(y_to_wy[y] for y in ys))
 
-    ## trans dictionary
-    pickle.dump(x_to_trans, open('outputs/{}-{}/{}-{}.trans.pkl'.format(lcode1, lcode2, lcode1, lcode2), 'wb'))
-    pickle.dump(y_to_trans, open('outputs/{}-{}/{}-{}.trans.pkl'.format(lcode1, lcode2, lcode2, lcode1), 'wb'))
+if lcode1=="en": _sanity_check(wx_to_x, x_to_ys, x_to_trans, y_to_wy)
+elif lcode2=="en": _sanity_check(wy_to_y, y_to_xs, y_to_trans, x_to_wx)
 
-    print("Done!")
+print("# Write")
+## lexicons
+pickle.dump((wx_to_x, x_to_wx), open('outputs/{}-{}/{}.lexicon.pkl'.format(lcode1, lcode2, lcode1), 'wb'))
+pickle.dump((wy_to_y, y_to_wy), open('outputs/{}-{}/{}.lexicon.pkl'.format(lcode1, lcode2, lcode2), 'wb'))
+
+## trans dictionary
+pickle.dump(x_to_trans, open('outputs/{}-{}/{}-{}.trans.pkl'.format(lcode1, lcode2, lcode1, lcode2), 'wb'))
+pickle.dump(y_to_trans, open('outputs/{}-{}/{}-{}.trans.pkl'.format(lcode1, lcode2, lcode2, lcode1), 'wb'))
+
+print("Done!")
